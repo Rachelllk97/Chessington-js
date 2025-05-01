5ac508234fcadff362ae4fefc5e8ba06974a63cf
Rachel Kelly <rachel.kelly@softwire.com>
2025-05-01 12:09:51 +0100

    first queen test passing

--- src/engine/pieces/king.js
+++ src/engine/pieces/king.js
@@ -6,6 +6,21 @@ export default class King extends Piece {
     }
 
     getAvailableMoves(board) {
-        return new Array(0);
+        const kingMoves = [];
+        let location = board.findPiece(this);
+        let square = Square.at(row, col);
+        
+        let directions = [
+            [-1, -1], [-1, 0], [-1, 1] // up+left, up, up+right
+            [0, -1], [0, 1] // left, right
+            [1, -1], [1, 0], [1, 1] // down+left, down, down_right
+        ];
+
+        for (let i = 0; i <directions.length; i++) {
+            let row = location.row + directions[i][0];
+            let col = location.col + directions[i][1];
+        }
+
+
     }
 }
--- tests/engine/pieces/knight.spec.js
+++ tests/engine/pieces/knight.spec.js
@@ -1,4 +1,7 @@
 import Knight from '../../../src/engine/pieces/knight';
+import Board from '../../../src/engine/board';
+import Player from '../../../src/engine/player';
+import Square from '../../../src/engine/square';
 
 describe('Knight', () => {
 
